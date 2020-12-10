82e0a88e6e21ee2e340fa4fe414fa9720299286f
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-12-11 02:15:02 +0900

    css수정

---
+++ website-animation-trick-master/website-animation-trick-master/member.sql
@@ -0,0 +1,10 @@
+create table member(
+    id int(10) not null,
+    password text not null,
+    name int not null,
+    mail varchar(20),
+    age int not null
+    primary key(id)
+);
+
+insert into member va
